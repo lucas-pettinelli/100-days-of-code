a9148899f4a0a9ed35e8cf02c6d52b9fd7327453
lucas-pettinelli <pettinellilucas@gmail.com>
2024-10-14 22:00:48 -0300

    Day 1 Hello World in 100 days of code in java

---
+++ java/day1-HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+//Day 1 - Hello World
+/*1. Write a program that prints "Hello World!" to the console
+*2. Add single line and multi-line comments to your code
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
