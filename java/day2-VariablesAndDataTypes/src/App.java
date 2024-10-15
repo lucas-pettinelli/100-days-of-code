ae3b95c47bd27ba5598ae9fbf05f5f6982a25fe7
lucas-pettinelli <pettinellilucas@gmail.com>
2024-10-14 22:06:56 -0300

    Day 2 Variables and Data types  in 100 days of code in java

---
+++ java/day2-VariablesAndDataTypes/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 1; // primitivo
+        Integer a = 1; // Wrapper
+
+        double y = 9.99;
+        Double b = 9.99;
+
+        String z = "Amor";
+
+        System.out.println("Minhas primeiras variáveis e tipo de dados são: ");
+        System.out.println("Tipo de numeros inteiros: " + x + ", " + a);
+        System.out.println("Tipo ponto flutuante: " + y + ", " + b);
+        System.out.println("Tipo String: " + z);
+
+    }
+}
